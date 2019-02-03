beb23ba94c81d47a5117bf1e0d0e7024fbca9cfe
Минка Киркова <46894463+MinkaKirkova@users.noreply.github.com>
2019-02-03 13:27:06 +0200

    Add files via upload

---
+++ brsys10to2and10to16/brsys10to2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace brsystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//Превръщане от десетично в двоично число
+            int n; string s = "";
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                int r = n % 2;
+                s = s + r.ToString();
+                n = n / 2;
+            }
+            //Console.WriteLine(s);
+
+            for (int i = s.Length - 1; i >= 0; i--)
+                Console.Write(s[i]);
+            //Втори начин на дес в двоично без низове
+/*
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+       
+            long S = 0;
+            long n = 1;
+
+                while (a != 0)
+                {
+                    S = S + a % 2 * n;
+                    n = n * 10;
+                    a = a / 2;
+                }
+                Console.WriteLine(S);
+        }
+*/
+            //Превръщане от десетично в шестнадесетично число
+            Console.WriteLine();
+            int n1; string s1 = "";
+            n1 = int.Parse(Console.ReadLine());
+            while (n1 > 0)
+            {
+                int r1 = n1 % 16;
+                if (r1 < 10)
+                    s1 = s1 + r1.ToString();
+                else
+                    s1 = s1 + (char)(r1 + 55);
+                n1 = n1 / 16;
+            }
+            //Console.WriteLine(s);
+
+            for (int j = s1.Length - 1; j >= 0; j--)
+                Console.Write(s1[j]);
+
+            Console.WriteLine();
+        }
+    }
+}
+ 
---
+++ brsys2to10and2to16/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace brsystem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Превръщане от двоично в десетично число
+            int n = 0, result = 0; int s;
+            s = int.Parse(Console.ReadLine());
+            while(s>0)
+            {
+                result = result + (int)(s%10 * Math.Pow(2, n));
+                n++;
+                s /= 10;
+            }
+             Console.WriteLine(result);
+            //Превръщане от двоично в шестнадесетично число
+            string bin = Console.ReadLine();
+            string hex = "";
+            while(bin.Length>=4)
+            { string sub = bin.Substring(bin.Length - 4, 4);
+            switch(sub)
+                { case "0000": hex += "0"; break;
+                case "0001": hex += "1"; break;
+                case "0010": hex += "2"; break;
+                case "0011": hex += "3"; break;
+                case "0100": hex += "4"; break;
+                case "0101": hex += "5"; break;
+                case "0110": hex += "6"; break;
+                case "0111": hex += "7"; break;
+                case "1000": hex += "8"; break;
+                case "1001": hex += "9"; break;
+                case "1010": hex += "A"; break;
+                case "1011": hex += "B"; break;
+                case "1100": hex += "C"; break;
+                case "1101": hex += "D"; break;
+                case "1110": hex += "E"; break;
+                case "1111": hex += "F"; break;
+                }
+                bin = bin.Substring(0, bin.Length - 4);
+                
+            }
+            // На остатък след цепенето добавяме нужния брой нули отпред
+            switch (bin.Length)
+            {
+                case 0: bin = "0000"; break;
+                case 1: bin = "000" + bin; break;
+                case 2: bin = "00" + bin; break;
+                case 3: bin = "0" + bin; break;
+            }
+            switch (bin)
+            {
+                case "0000": hex += "0"; break;
+                case "0001": hex += "1"; break;
+                case "0010": hex += "2"; break;
+                case "0011": hex += "3"; break;
+                case "0100": hex += "4"; break;
+                case "0101": hex += "5"; break;
+                case "0110": hex += "6"; break;
+                case "0111": hex += "7"; break;
+                case "1000": hex += "8"; break;
+                case "1001": hex += "9"; break;
+                case "1010": hex += "A"; break;
+                case "1011": hex += "B"; break;
+                case "1100": hex += "C"; break;
+                case "1101": hex += "D"; break;
+                case "1110": hex += "E"; break;
+                case "1111": hex += "F"; break;
+            }
+            for(int k=hex.Length-1;k>=0;k++)
+            Console.Write(hex[k]);
+            Console.WriteLine();
+        }
+
+
+
+    }
+
+}
+
